31fbdf827a453aabf418a6c49a65fc2093021a84
jchacks <chacksfieldj@gmail.com>
2023-08-06 18:07:21 +0200

    Fix imports

--- robodog/server/cli.py
+++ robodog/server/cli.py
@@ -1,14 +1,14 @@
 import click
 
-from robodog import server
-
 
 @click.command()
 @click.option("--ip", help="IP of host to stream to.")
 @click.option("--port", default=5000, type=int, help="Port to stream to.")
 def camera(ip, port):
-    """Simple program that greets NAME for a total of COUNT times."""
-    server.camera.udp_stream(ip, port)
+    """Run camera."""
+    from robodog.server import camera
+
+    camera.udp_stream(ip, port)
 
 
 if __name__ == "__main__":
