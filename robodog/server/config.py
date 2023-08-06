28b6287d309731d57e79d8d4877a927fb3738a40
jchacks <chacksfieldj@gmail.com>
2023-08-06 11:37:33 +0200

    Fixed logging level

--- robodog/server/config.py
+++ robodog/server/config.py
@@ -5,12 +5,15 @@ _LOGGER: Optional[logging.Logger] = None
 LOGGING_FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 
 
-def configure_logging():
+def configure_logging(level=logging.INFO):
     global _LOGGER
     if _LOGGER is None:
         _LOGGER = logging.getLogger()
+        _LOGGER.setLevel(level)
+
         streamhandler = logging.StreamHandler()
         formatter = logging.Formatter(LOGGING_FORMAT)
         streamhandler.setFormatter(formatter)
         _LOGGER.addHandler(streamhandler)
+
     return _LOGGER
